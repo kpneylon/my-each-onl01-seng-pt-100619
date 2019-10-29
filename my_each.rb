e585f046d387cc6e5d50e88e71fb6beb39d0287a
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-10-29 03:02:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,8 @@
-def my_each(array) # put argument(s) here
-  count = 0
-  while count < array.length do |i|
-    count += 1
-  end  
-  # code here
+def my_each(array)
+  i = 0
+  while i < array.length
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
+  end
 end
